c45b27ec18f7784a0ed01c30f930d4be46462902
Mihai-Valentin DUMITRU <mihai.dumitru2201@stud.acs.upb.ro>
2019-02-21 23:58:20 +0200

    add 'ssconvert missing' hint

--- xls2csv.sh
+++ xls2csv.sh
@@ -5,6 +5,11 @@ if [ $# -ne 1 ] || [ ! -d "$1" ]; then
 	exit 1
 fi
 
+if ! command -v ssconvert &> /dev/null; then
+	echo 'ssconvert missing (search for the "gnumeric" package)' 1>&2
+	exit 1
+fi
+
 pushd "$1" &> /dev/null
 IFS=$'\n'
 for file in *.xls; do
